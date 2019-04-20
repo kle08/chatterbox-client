7b0d8727373b847441c074e91e868260ee6d0b7f
kle08 <kevinle1991@gmail.com>
2019-04-19 17:15:19 -0700

    First pass for chatterbox

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,26 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    // FormView.$form.on('submit', FormView.handleSubmit);
+    MessagesView.render();
   },
 
   render: function() {
+    var html = '';
+    $.getJSON(Parse.server, function(data) {
+      for (let i = 0; i < data.results.length; i++) {
+        if (data.results[i].username) {
+          html += MessageView.render(data.results[i]);
+        }
+        // console.log(data.results[i])
+      }
+      $('#chats').append(html);
+    });
+    // console.log(allData)
   }
+
+  // renderMessage: function() {
+
+  // }
 
 };
